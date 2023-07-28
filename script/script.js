9c7a3ceae4a14e5a850bbc86b211453e7670838f
soyeon <qkrthdus0514@gmail.com>
2023-07-28 17:36:46 +0900

    slide

--- script/script.js
+++ script/script.js
@@ -24,3 +24,32 @@ mNav.addEventListener('click',function(){
 close.addEventListener('click',function(){
     mNavOpen.style.display = 'none';
 });
+
+// main-swiper-slide
+// const 변수명 = new Swiper('적용대상')
+// const 변수명 = new Swiper('적용대상,'{속성:값, 속성:값})
+const pawin_slide = new Swiper('#pawin_slide',{
+    //자동재생
+    autoplay:{
+        delay:3000, //슬라이드 간격(밀리초) 기본3초
+    disableOnInteraction:false,//버튼 클릭 후 자동재생 유지
+    },
+    loop:true, //자연스럽게 한 방향으로 넘어가듯이 하기.
+    effect:'fade', //제자리 변경 슬라이드 효과
+    //direction:'vertical', //방향 아래로 바꾸기.
+    navigation: {
+        nextEl: '#pawin_slide .swiper-button-next',
+        prevEl: '#pawin_slide .swiper-button-prev',
+    },
+});
+const test = new Swiper('#test',{
+    autoplay:{delay:1000},
+    direction:'vertical',
+    navigation: {//이전, 다음 내비게이션 연결
+        //next, prev 객체 연결 시 부모를 안적으면
+        //body 안에 있는 모든 swiper-next,prev를 인식하기 때문에
+        //개별인식가능한 부모이름을 반드시 앞에 먼저 작성한다.
+        nextEl: '#test .swiper-button-next',
+        prevEl: '#test .swiper-button-prev',
+    }
+})
